c16591085ccfb05997172b0d94034936060d8ddc
Alexandra Rodriguez-Beuerman <arbeuerman@gmail.com>
2021-02-23 22:48:59 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,9 @@ def join_nested_strings(src)
   while outer_array_counter <  src.length do 
     inner_array_counter = 0 
     while inner_array_counter <  src[outer_array_counter].length do 
-      if 
+      if src[outer_array_counter][inner_array_counter].is_s 
+        sentence += src[outer_array_counter][inner_array_counter]
+      end 
       inner_array_counter += 1 
     end 
     outer_array_counter += 1 
